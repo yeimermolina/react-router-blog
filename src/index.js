4e3d1773d3176c3c749b095fee83a751c3af9cdd
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-11-10 17:57:05 -0400

    Add react router and first component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
+import PostsIndex from './components/posts_index';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+    
   </Provider>
   , document.querySelector('.container'));
