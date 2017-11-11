b2f9b0e384e124c697dc56e33c427c9631ffa5d8
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-11-10 21:14:07 -0400

    Fetch lists of post from api when loading index page

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import promise from 'redux-promise';
+import ReduxPromise from 'redux-promise';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import PostsIndex from './components/posts_index';
 import reducers from './reducers';
 
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(...reducers, ReduxPromise),
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     
     <BrowserRouter>
       <div>
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,7 +5,7 @@ export default function(state = {}, action){
   switch (action.type) {
     case FETCH_POSTS:
       console.log(action.payload.data)
-      return _.mapKeys(actions.payload.data, 'id')
+      return _.mapKeys(action.payload.data, 'id')
     default:
       return state;
   }
